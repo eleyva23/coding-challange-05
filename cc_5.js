9bcc55bc57dffba25fc06623a6e89257309fc034
emma <eleyva2@usf.edu>
2025-09-23 13:20:42 -0700

    step 2 is complete added a onject array

---
+++ cc_5.js
@@ -0,0 +1,7 @@
+// Step 2 Creating an Array of Employee Objects 
+const employees= [
+    {name:"Emma Leyva", hourlyRate: 20, hoursWorked: 25},
+    {name: "Sophia Leyva", hourlyRate: 17, hoursWorked: 15},
+    {name: "Jae Diehl", hourlyRate: 14, hoursWorked: 10 },
+];
+// Step 3 Creating a Function 
