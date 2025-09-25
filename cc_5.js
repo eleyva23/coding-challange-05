6ad73ba5130e99ec7fc0e56809f45efe5de44f40
emma <eleyva2@usf.edu>
2025-09-25 14:18:52 -0700

    chnaged hours in step 1

--- cc_5.js
+++ cc_5.js
@@ -1,15 +1,16 @@
 // Step 2 Creating an Array of Employee Objects 
 const employees= [
-    {name:"Emma Leyva", hourlyRate: 20, hoursWorked: 25},
+    {name:"Emma Leyva", hourlyRate: 20, hoursWorked: 50},
     {name: "Sophia Leyva", hourlyRate: 17, hoursWorked: 15},
-    {name: "Jae Diehl", hourlyRate: 14, hoursWorked: 10 },
+    {name: "Jae Diehl", hourlyRate: 14, hoursWorked: 10 }
 ];
 // Step 3 Creating a Function 
-function calculateBasePay (rate, hours) {
-    const baseHours= Math.min(hours, 40);
-    
-    const basePay= rate * baseHours;
-    
-    return basePay;
+function calculateBasePay(rate, hours) {
+  if (hours <= 40) {
+    return rate * hours;
+  } else {
+    return rate * 40; // Only pay for the first 40 hours
+  }
 }
-console.log(calculateBasePay(rate,hours))
+console.log(calculateBasePay(20, 25)) // output 500
+// Step 4 
